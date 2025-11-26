a9a3adcff1395708192509287a53356f01f70139
Luan <luancarloslaurentino52@gmail.com>
2025-11-25 23:35:06 -0300

    adiciona as novas funções no main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # src/main.py
 from process_data import ler_arquivo, validar_dados
-
+from process_calculos import gerar_tabelas_resumo
 def main():
     caminho = "data/notas_alunos.xlsx"  # arquivo de exemplo
     try:
@@ -8,6 +8,12 @@ def main():
         df = validar_dados(df)
         print("Arquivo lido com sucesso!")
         print(df.head())
+
+        resumo = gerar_tabelas_resumo(df)
+        print("\nMédias por aluno:")
+        print(resumo["media_aluno"])
+        print("\nMédias por disciplina:")
+        print(resumo["media_disciplina"])
     except Exception as e:
         print(f"Erro: {e}")
 
